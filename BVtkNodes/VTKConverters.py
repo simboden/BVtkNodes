db4c054b5eefd154f74e652c62bc0da4d70561b2
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2019-05-04 09:13:47 +0300

    Add comments, clean up and restructure VTKConverters.py

--- BVtkNodes/VTKConverters.py
+++ BVtkNodes/VTKConverters.py
@@ -1,19 +1,22 @@
 from .update import *
 from .core import *
 import bmesh
-TYPENAMES = []
 
-# ----------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Converters from VTK to Blender
+# -----------------------------------------------------------------------------
+
 class VTK2Blender(Node, VTKNode):
-
+    '''Output VTK Node to Blender Mesh Object'''
     bl_idname = 'VTK2BlenderType' # type name
-    bl_label  = 'ToBlender'       # label for nice name display
+    bl_label  = 'VTK To Blender'  # label for nice name display
 
     def start_scan(self, context):
         if context:
             if self.auto_update:
-                bpy.ops.vtk.auto_update_scan(node_name=self.name,
-                                             tree_name=context.space_data.node_tree.name)
+                bpy.ops.vtk.auto_update_scan(
+                    node_name=self.name,
+                    tree_name=context.space_data.node_tree.name)
 
     m_Name = bpy.props.StringProperty(name='Name', default='mesh')
     auto_update = bpy.props.BoolProperty(default=False, update=start_scan)
@@ -35,6 +38,7 @@ class VTK2Blender(Node, VTKNode):
         layout.operator("node.update", text="update").node_path = node_path(self)
 
     def update_cb(self):
+        '''Update node'''
         input_node, vtkobj = self.get_input_node('input')
         ramp = None
         if input_node and input_node.bl_idname == 'VTKColorMapperType':
@@ -49,63 +53,112 @@ class VTK2Blender(Node, VTKNode):
     def apply_properties(self, vtkobj):
         pass
 
-add_class(  VTK2Blender )
-TYPENAMES.append( 'VTK2BlenderType' )
 
+def vtkdata_to_blender(data, name, ramp=None, smooth=False):
+    '''Convert VTK data to Blender mesh object, using optionally
+    given color ramp and normal smoothing.
+    '''
+    if not data:
+        print('vtkdata_to_blender -- no data!')
+        return
+    if issubclass(data.__class__, vtk.vtkImageData):
+        imgdata_to_blender(data, name)
+        return
+    me, ob = mesh_and_object(name)
+    if me.is_editmode:
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+    err = 0 # Number of errors encountered
+    bm = bmesh.new()
+    # bm.from_mesh(me) # fill it in from a Mesh
 
-# ----------------------------------------------------------------
-menu_items = [ NodeItem(x) for x in TYPENAMES ]
-CATEGORIES.append( VTKNodeCategory("converters", "converters", items=menu_items) )
+    # Get vertices
+    data_p = data.GetPoints()
+    verts = [bm.verts.new(data_p.GetPoint(i)) for i in range(data.GetNumberOfPoints())]
 
-# ---------------------------------------------------------------------------------
-# OperatorNodeUpdate
-# ---------------------------------------------------------------------------------
-
-class OperatorNodeUpdate(bpy.types.Operator):
-    bl_idname = "node.update"
-    bl_label = "update"
-    node_path = bpy.props.StringProperty()
-    use_queue = bpy.props.BoolProperty(default = True)
-
-    def execute(self, context):
-        check_cache()
-        node = eval(self.node_path)
-        if node:
-            print('Updating from: '+node.name)
-            if self.use_queue:
-                Update(node, node.update_cb)
+    # Loop over cells to create edges and faces
+    for i in range(data.GetNumberOfCells()):
+        data_pi = data.GetCell(i).GetPointIds()
+        # Error is raised if a face already exists. Count errors.
+        try:
+            face_verts = [verts[data_pi.GetId(x)] for x in range(data_pi.GetNumberOfIds())]
+            if len(face_verts) == 2:
+                e = bm.edges.new(face_verts)
             else:
-                no_queue_update(node, node.update_cb)
-        self.use_queue = True
-        return {'FINISHED'}
+                f = bm.faces.new(face_verts)
+                f.smooth = smooth
+        except:
+            err += 1
+    if err:
+        print('num err', err)
 
-add_class(OperatorNodeUpdate)
+    # Set normals
+    point_normals = data.GetPointData().GetNormals()
+    cell_normals = data.GetCellData().GetNormals()
+    if cell_normals:
+        bm.faces.ensure_lookup_table()
+        for i in range(len(bm.faces)):
+            bm.faces[i].normal = cell_normals.GetTuple(i)
+    if point_normals:
+        for i in range(len(verts)):
+            verts[i].normal = point_normals.GetTuple(i)
 
-# ---------------------------------------------------------------------------------
-# Auto Update Scan
-# ---------------------------------------------------------------------------------
+    # Set colors and color legend
+    if ramp and ramp.color_by:
+        texture = ramp.get_texture()
+        if ramp.texture_type == 'IMAGE':
+            img = image_from_ramp(texture.color_ramp, texture.name+'IMAGE', 1000)
+            texture = get_item(bpy.data.textures, texture.name+'IMAGE', 'IMAGE')
+            texture.image = img
+        texture_material(me, 'VTK'+name, texture)
+        color_by = ramp.color_by
+        vrange = (ramp.min, ramp.max)
+        if ramp.lut:
+            create_lut(name, vrange, 6, texture, h=ramp.height)
+        bm.verts.index_update()
+        bm.faces.index_update()
+        if color_by[0] == 'P':
+            bm = point_unwrap(bm, data, int(color_by[1:]), vrange)
+        else:
+            bm = face_unwrap(bm, data, int(color_by[1:]), vrange)
+
+    bm.to_mesh(me)  # store bmesh to mesh
+
+    print('vtkdata_to_blender -- ok!  -- num verts =', len(verts))
+
+    #if (autoCenter):
+    #    bpy.data.objects[me.name].select = True
+    #    context.scene.objects.active = bpy.data.objects[me.name]
+    #    bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN')
+
+
+# -----------------------------------------------------------------------------
+# Auto update scan functions
+# -----------------------------------------------------------------------------
 
 def map(node, pmap = None):
-    """ returns a map, which represent
-    the status (m_properties and inputs) of
-    every node connected to the one given.
-    {} map:   node name -> (nodeprops, nodeinputs)
-    {} nodeprops:  property name -> property value
-    {} nodeinputs: input name -> connected node name """
+    '''Create node property map (pmap). The map represent the status
+    (m_properties and inputs) of every preceding node connected
+    to node.
+    '''
+    # {} map:        node name -> (nodeprops, nodeinputs)
+    # {} nodeprops:  property name -> property value
+    # {} nodeinputs: input name -> connected node name
+
     if not pmap:
         pmap = {}
     props = {}
     for prop in node.m_properties():
         val = getattr(node, prop)
+        # Special for arrays. Any other type to include?
         if val.__class__.__name__ == 'bpy_prop_array':
-            # this is for arrays. Is there
-            # any other strange bpy type?
             val = [x for x in val]
         props[prop] = val
+
     if hasattr(node, 'special_properties'):
         # you can add to a node a function called special_properties
         # to make auto update notice differences outside of m_properties
         props['special_properties'] = node.special_properties()
+
     links = {}
     for input in node.inputs:
         links[input.name] = ''
@@ -117,6 +170,7 @@ def map(node, pmap = None):
 
 
 def differences(map1, map2):
+    '''Generate differences in properties and inputs of argument maps'''
     props = {}   # differences in properties
     inputs = {}  # differences in inputs
     for node in map1:
@@ -136,8 +190,7 @@ def differences(map1, map2):
 
 
 def compare(dict1, dict2):
-    """ compares two dictionaries and returns
-    a list of mismatching keys """
+    '''Compare two dictionaries. Return a list of mismatching keys'''
     diff = []
     for k in dict1:
         if k not in dict2:
@@ -154,7 +207,7 @@ def compare(dict1, dict2):
 
 
 class VTKAutoUpdateScan(bpy.types.Operator):
-    """ . """
+    '''VTK Auto Update Scan'''
     bl_idname = "vtk.auto_update_scan"
     bl_label = "auto update"
     _timer = None
@@ -178,7 +231,10 @@ class VTKAutoUpdateScan(bpy.types.Operator):
                 return {'CANCELLED'}
         return {'PASS_THROUGH'}
 
-    def node_is_valid(self):    # returns false if node has been deleted or auto update has been turned off
+    def node_is_valid(self):
+        '''Node validity test. Return false if node has been deleted or auto
+        update has been turned off.
+        '''
         return self.node.name in self.tree and self.node.auto_update
 
     def execute(self, context):
@@ -195,96 +251,22 @@ class VTKAutoUpdateScan(bpy.types.Operator):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
 
-add_ui_class(VTKAutoUpdateScan)
-
-# ---------------------------------------------------------------------------------
-# vtkdata_to_blender
-# ---------------------------------------------------------------------------------
 
 
-def vtkdata_to_blender(data, name, ramp=None, smooth=False):
-    """ convert the given vtkdata
-    creating or overwriting a blender object named 'name' """
-    if not data:
-        print('vtkdata_to_blender -- no data!')
-        return
-    if issubclass(data.__class__, vtk.vtkImageData):
-        imgdata_to_blender(data, name)
-        return
-    me, ob = mesh_and_object(name)
-    if me.is_editmode:
-        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-    err = 0
-    bm = bmesh.new()
-    # bm.from_mesh(me) # fill it in from a Mesh
-
-    # set vertices and faces:
-    data_p = data.GetPoints()
-    verts = [bm.verts.new(data_p.GetPoint(i)) for i in range(data.GetNumberOfPoints())]
-    for i in range(data.GetNumberOfCells()):
-        data_pi = data.GetCell(i).GetPointIds()
-        try:
-            # it complains if a face is already existing
-            face_verts = [verts[data_pi.GetId(x)] for x in range(data_pi.GetNumberOfIds())]
-            if len(face_verts) == 2:
-                e = bm.edges.new(face_verts)
-            else:
-                f = bm.faces.new(face_verts)
-                f.smooth = smooth
-        except:
-            err += 1
-    if err:
-        print('num err', err)
-
-    # set normals
-    point_normals = data.GetPointData().GetNormals()
-    cell_normals = data.GetCellData().GetNormals()
-    if cell_normals:
-        bm.faces.ensure_lookup_table()
-        for i in range(len(bm.faces)):
-            bm.faces[i].normal = cell_normals.GetTuple(i)
-    if point_normals:
-        for i in range(len(verts)):
-            verts[i].normal = point_normals.GetTuple(i)
-
-    # set colors and lut
-    if ramp and ramp.color_by:
-        texture = ramp.get_texture()
-        if ramp.texture_type == 'IMAGE':
-            img = image_from_ramp(texture.color_ramp, texture.name+'IMAGE', 1000)
-            texture = get_item(bpy.data.textures, texture.name+'IMAGE', 'IMAGE')
-            texture.image = img
-        texture_material(me, 'VTK'+name, texture)
-        color_by = ramp.color_by
-        Range = (ramp.min, ramp.max)
-        if ramp.lut:
-            create_lut(name, Range, 6, texture, h=ramp.height)
-        bm.verts.index_update()
-        bm.faces.index_update()
-        if color_by[0] == 'P':
-            bm = point_unwrap(bm, data, int(color_by[1:]), Range)
-        else:
-            bm = face_unwrap(bm, data, int(color_by[1:]), Range)
-
-    bm.to_mesh(me)  # store bmesh to mesh
-
-    print('vtkdata_to_blender -- ok!  -- num verts =', len(verts))
-
-    #if (autoCenter):
-    #    bpy.data.objects[me.name].select = True
-    #    context.scene.objects.active = bpy.data.objects[me.name]
-    #    bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN')
+# -----------------------------------------------------------------------------
+# Help functions
+# -----------------------------------------------------------------------------
 
 
 def mesh_and_object(name):
-    """ method gets/creates an object and his mesh and returns both """
+    '''Get mesh object and mesh'''
     me = get_item(bpy.data.meshes, name)
     ob = get_object(name, me)
     return me, ob
 
 
 def get_item(data, *args):
-    """ method gets/creates the item with key args[0] from data and return it """
+    '''Get or create item with key args[0] from data'''
     item = data.get(args[0])
     if not item:
         item = data.new(*args)
@@ -292,30 +274,31 @@ def get_item(data, *args):
 
 
 def set_link(data, item):
-    """ method links item to data if item isn't already linked """
+    '''Link item to data'''
     if item.name not in data:
         data.link(item)
 
 
 def get_object(name, data):
-    """ method gets/creates object, sets his data, adds it to current scene """
+    '''Initialize mesh object name with data into current scene'''
     ob = get_item(bpy.data.objects, name, data)
     ob.data = data
     set_link(bpy.context.scene.objects, ob)
     return ob
 
 
-def texture_material(me, name, texture = None, texturetype = 'IMAGE'):
-    """ method gets/creates a material and link to it the given texture,
-    then apply it to given mesh. If texture isn't given one is created
-    """
+def texture_material(me, name, texture=None, texturetype='IMAGE'):
+    '''Get or create a material and link with given texture,
+    then apply it to given object.
+    '''
     if not texture:
         texture = get_item(bpy.data.textures, name, texturetype)
         texture.type = texturetype
     mat = get_item(bpy.data.materials, name)
     if mat.name not in me.materials:
         me.materials.append(mat)
-    for ts in mat.texture_slots:  # disabling all other textures
+    # Disable other textures
+    for ts in mat.texture_slots:
         if ts:
             ts.use = False
     if texture.name not in mat.texture_slots:
@@ -328,9 +311,7 @@ def texture_material(me, name, texture = None, texturetype = 'IMAGE'):
     return texture, mat
 
 def image_from_ramp(ramp, name, l):
-    """ takes a color ramp and creates a blender image 1 pixel
-    tall and l pixels wide
-    """
+    '''Create image (size 1 px) from color ramp'''
     img = get_item(bpy.data.images, name, l, 1)
     p = []
     for j in range(l):
@@ -339,10 +320,11 @@ def image_from_ramp(ramp, name, l):
     return img
 
 
-def face_unwrap(bm, data, array_index, Range):
+def face_unwrap(bm, data, array_index, vrange):
+    '''Unwrap by cell data'''
     scalars=data.GetCellData().GetArray(array_index)
     if scalars is not None:
-        min, max = Range
+        min, max = vrange
         if max==min:
             print("can't unwrap -- values are constant -- range(",min,",",max,")!")
             return bm
@@ -354,10 +336,11 @@ def face_unwrap(bm, data, array_index, Range):
     return bm
 
 
-def point_unwrap(bm, data, array_index, Range):
+def point_unwrap(bm, data, array_index, vrange):
+    '''Unwrap by point data'''
     scalars=data.GetPointData().GetArray(array_index)
     if scalars is not None:
-        min, max = Range
+        min, max = vrange
         if max == min:
             print("can't unwrap -- values are constant -- range(",min,",",max,")!")
             return bm
@@ -369,13 +352,12 @@ def point_unwrap(bm, data, array_index, Range):
     return bm
 
 
-# ---------------------------------------------------------------------------------
-# scalarbar to blender
-# ---------------------------------------------------------------------------------
-
+# -----------------------------------------------------------------------------
+# Color legend
+# -----------------------------------------------------------------------------
 
 def text(name, body):
-    """ method gets/creates a text object """
+    '''Get a text data block'''
     font = get_item(bpy.data.curves, name, 'FONT')
     ob = get_object(name, font)
     font.body = body
@@ -383,6 +365,7 @@ def text(name, body):
 
 
 def delete_texts(name):
+    '''Delete text data block'''
     for ob in bpy.data.objects:
         if ob.name.startswith(name):
             curve = ob.data
@@ -390,10 +373,12 @@ def delete_texts(name):
             bpy.data.curves.remove(curve)
 
 
-def create_lut(name, Range, n_div, texture, b=0.5, h=5.5, x=5, y=0, z=0, fontsize=0.35, roundto=2):
-    """ method creates a lookuptable and adds it to current scene """
-    name = name+'_colormap'
-    delete_texts(name+'_lab')
+def create_lut(name, vrange, n_div, texture, b=0.5, h=5.5, x=5, y=0, z=0, fontsize=0.35, roundto=2):
+    '''Create value labels and color legends and add to current scene'''
+    name = name + '_colormap'
+    delete_texts(name + '_lab') # Delete old labels
+
+    # Create plane and UVs
     plane = bmesh.new()
     plane.faces.new((
         plane.verts.new((0, 0, 0)),
@@ -411,9 +396,11 @@ def create_lut(name, Range, n_div, texture, b=0.5, h=5.5, x=5, y=0, z=0, fontsiz
     plane.to_mesh(me)
     ob.location = x,y,z
     texture_material(me, name, texture)
-    min, max = Range
-    if min>max or h<=0:
-        print('Range maximum greater than minimum')
+
+    # Calculate label interval
+    min, max = vrange
+    if min > max or h <= 0:
+        print('vrange maximum greater than minimum')
         return
     import math
     idealspace = (max-min)/(h)
@@ -431,6 +418,8 @@ def create_lut(name, Range, n_div, texture, b=0.5, h=5.5, x=5, y=0, z=0, fontsiz
         return
     starth = (h*(start-min))/delta
     steph = (h*step)/delta
+
+    # Add labels as texts
     for i in range(int(math.floor((max-start)/step))+1):
         t = text(name+'_lab'+str(i), '{:.15}'.format(start+i*step))
         t.data.size = fontsize
@@ -440,17 +429,17 @@ def create_lut(name, Range, n_div, texture, b=0.5, h=5.5, x=5, y=0, z=0, fontsiz
         t.parent = ob
 
 
-# ---------------------------------------------------------------------------------
-#  imgdata_to_blender
-# ---------------------------------------------------------------------------------
-
+# -----------------------------------------------------------------------------
+#  Image data conversion
+# -----------------------------------------------------------------------------
 
 def imgdata_to_blender(data, name):
-    """ Takes a vtkImageData and stores
-    it as an image in blender memory
-    """
+    '''Convert vtkImageData to a Blender image'''
+
     wm = bpy.context.window_manager
     scalars = data.GetPointData().GetScalars()
+
+    # Generate image data to img
     dim = data.GetDimensions()
     z = 0
     wm.progress_begin(0, 100)
@@ -477,6 +466,8 @@ def imgdata_to_blender(data, name):
     img.pixels = p
     wm.progress_end()
     print('imgdata_to_blender -- ok! -- num pixels = '+str(l))
+
+    # Create plane mesh with UVs to show the image
     spacing = data.GetSpacing()
     x = dim[0] * spacing[0]
     y = dim[1] * spacing[0]
@@ -500,5 +491,35 @@ def imgdata_to_blender(data, name):
     mat.use_shadeless = True
     tex.image = img
 
+
+class OperatorNodeUpdate(bpy.types.Operator):
+    '''Node Update Operator'''
+    bl_idname = "node.update"
+    bl_label = "update"
+    node_path = bpy.props.StringProperty()
+    use_queue = bpy.props.BoolProperty(default = True)
+
+    def execute(self, context):
+        check_cache()
+        node = eval(self.node_path)
+        if node:
+            print('Updating from: '+ node.name)
+            if self.use_queue:
+                Update(node, node.update_cb)
+            else:
+                no_queue_update(node, node.update_cb)
+        self.use_queue = True
+        return {'FINISHED'}
+
+
+# Add classes and menu items
+TYPENAMES = []
+add_class(VTK2Blender)
+TYPENAMES.append('VTK2BlenderType')
+menu_items = [NodeItem(x) for x in TYPENAMES]
+CATEGORIES.append(VTKNodeCategory("converters", "converters", items=menu_items))
+
+add_class(OperatorNodeUpdate)
+add_ui_class(VTKAutoUpdateScan)
 add_ui_class(VTKFunctionQueue)
 
