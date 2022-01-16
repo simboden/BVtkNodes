acfbb7dcea916330eded035d7a40713525f36e47
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2022-01-16 12:36:27 +0200

    Add disabling vtkRenderingMatplotlib in install_bvtk.sh
    - Trying to fix github CI failure with this

--- test/install_bvtk.sh
+++ test/install_bvtk.sh
@@ -3,6 +3,9 @@ $PWD/blender/2.93/python/bin/python3.9 -m ensurepip
 $PWD/blender/2.93/python/bin/python3.9 -m pip install --upgrade pip
 $PWD/blender/2.93/python/bin/python3.9 -m pip install vtk==9.1.0 pyvista
 
+#Disable vtkRenderingMatplotlib
+sed -i 's/from\ \.vtkRenderingMatplotlib\ import/\#from\ \.vtkRenderingMatplotlib\ import/g' $PWD/blender/2.93/python/lib/python3.9/site-packages/vtkmodules/all.py
+
 #Install the addon
 mkdir $PWD/blender/2.93/scripts/addons/BVtkNodes
 unzip BVtkNodes.zip -d $PWD/blender/2.93/scripts/addons/BVtkNodes
