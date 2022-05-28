7836292f3ce40c9f88247a147dea813acf58580f
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2022-05-28 14:03:22 +0300

    Fix bug in show/hide properties
    - __annotations__ work around for listing properties had stopped working
      in Blender 2.93. Now property names are taken directly from m_properties.

--- showhide_properties.py
+++ showhide_properties.py
@@ -27,20 +27,12 @@ class BVTK_PT_ShowHide_Properties(bpy.types.Panel):
         for i in range(len(m_properties)):
             row = layout.row()
             row.prop(active_node, "b_properties", index=i)
-
-            # There seems to be something strange with Python type annotations:
-            # getattr does not find annotations, so here is workaround.
-            # TODO: Is it a bug in Python 3.7.0? This is ugly.
-            # prop_dict = getattr(core.CLASSES[active_node.bl_idname], m_properties[i])[1] # OLD
+            # Take property name from m_properties
             mp = m_properties[i]
-            if mp in core.CLASSES[active_node.bl_idname].__annotations__:
-                prop_dict = core.CLASSES[active_node.bl_idname].__annotations__[mp][1]
-                if "name" in prop_dict:  # collection properties don't have name
-                    row.label(text=prop_dict["name"])
-                elif "attr" in prop_dict:
-                    row.label(text=prop_dict["attr"][2:])  # removing first chars 'm_'
-                else:
-                    l.error("Broken dict " + str(prop_dict))
+            if mp.startswith("m_"):
+                mp = mp[2:]
+            row.label(text=mp)
+
         # Custom code editing operators
         row = layout.row()
         row.operator("node.bvtk_custom_code_edit", text="Edit Custom Code")
